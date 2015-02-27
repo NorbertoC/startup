0043fe59e7086d683062a8661548fde6e4cdcdd8
NorbertoC <norberto.carosella@gmail.com>
2015-02-27 16:08:40 -0300

    JS - JQuery

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -1,36 +1,37 @@
 $(document).ready(function() {
-  $("section").hide(function() {
     $("section").fadeIn(1000);
-    $( ".alias" ).focus();
-  });
-});
+    $(".alias").focus();
 
-/*$("section").get( "http://localhost:3000/search?q=javascript", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
-});*/
+    $('.myButton').on('click', welcome);
 
+    function welcome() {
+      var alias = $('.alias').val();
+      var url = 'http://bootcamp.aws.af.cm/welcome/' + alias;
 
-function loadXMLDoc() {
+      $.get(url, function(data) {
+        console.log(data);
 
-  var sectionColor = document.getElementById("section");
-  var name = document.getElementById("alias").value;
-  var xmlhttp;
-  xmlhttp = new XMLHttpRequest();
-
-  if (document.getElementById("alias").value == "" || document.getElementById("alias").value == undefined) {
-    sectionColor.style.color = "#FF0000";
-  }
-  else {
-    sectionColor.style.color = "#000000";
-  }
-
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementById("section").innerHTML=xmlhttp.responseText;
+        if (data.response) {
+          $('section').html(data.response).css('color', 'green');
+        } else {
+          $('section').html(data).css('color', 'red');
+        }
+      });
     }
-  }
-  xmlhttp.open("GET","http://bootcamp.aws.af.cm/welcome/" + name, true);
-  xmlhttp.send();
-}
 
+    $.get('http://localhost:3000/search', {'q': 'html5'})
+      .done(function(data) {
+        console.log(data);
+        $.each(data.statuses, function(key, value) {
+          var template = '<article>' +
+            '<div class="user">' + value.user.screen_name + '</div>' +
+            '<div class="avatar"><img src="' + value.user.profile_image_url + '"/></div>' +
+            '<div class="text">' + value.text + '</div>' +
+            '<div class="date">' + value.created_at + '</div>' +
+            '<hr /></article>';
+          $('.tweets').append(template);
+          console.log(value);
+        });
+      });
+  //});
+});
