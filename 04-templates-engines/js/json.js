6085007f66e63e1be72fbf76f7093347ae5fa6a0
NorbertoC <norberto.carosella@gmail.com>
2015-03-05 22:29:37 -0300

    Templates

---
+++ 04-templates-engines/js/json.js
@@ -0,0 +1,38 @@
+var data = {
+        
+        person:{
+        
+            firstName: "Norberto", 
+            lastName: "Carosella",
+            birth: '08/04/1987',
+            gender: 'Male',
+            adress : 'Salguero 635 - 3/C',
+            mail: 'norberto.carosella@gmail.com',
+            phone: '3513928108'
+        },
+            jobTitle: "Developer",
+            location: 'Cordoba',
+            image: 'foto.jpg',
+            twitter: "@ncarosella",
+            pastJobs:
+            [{
+                job: "NOC Operator at Claro AR",
+                place: "Cordoba",
+                year: "2012-2015"
+            },
+            {
+                job: "Help Desk at Siglo 21 University",
+                place: "Cordoba",
+                year: "2011-2012"
+            }],
+            Education:
+            [{
+                place: 'Siglo 21 University'
+            },
+            {
+                place: 'Nueva Cordoba'
+
+            }]
+        
+     
+};
