0002866cb786a35174632aec1dd5d9b3e80c7cbe
Norberto Carosella <norberto.carosella@gmail.com>
2015-03-11 11:37:00 -0300

    last update

--- 05-backbone/js/backbone/Models/movie.js
+++ 05-backbone/js/backbone/Models/movie.js
@@ -3,28 +3,31 @@ var Movie = Backbone.Model.extend({
     save: function (o) {
         if (o.titulo === "") {
             alert("Nombre vacio!!");
-        }else {
-        var len = localStorage.length;
-        if (len === 0) {
-            localStorage.setItem("1", JSON.stringify(o));
-        }else {
-        var flag = 0;
-        for (x=0; x<=localStorage.length-1; x++)  {  
-            clave = localStorage.key(x);
-             moviel =(JSON.parse(localStorage.getItem(clave))) ;
-             if (moviel.titulo == o.titulo) {
-                    flag = 1;
-             }
+        }
+        else {
+            var len = localStorage.length;
+            if (len === 0) {
+                localStorage.setItem("1", JSON.stringify(o));
             }
-        if (flag==1) {
-            alert("Esa pelicula ya esta cargada!!");
-        }else{
-        var index = len - 1;
-        var key = parseInt(localStorage.key(index)) + 1;
-        console.log("save");
-        localStorage.setItem(key, JSON.stringify(o));
-        }
-        }
+            else {
+                var flag = 0;
+                for (x=0; x<=localStorage.length-1; x++)  {  
+                    clave = localStorage.key(x);
+                    moviel =(JSON.parse(localStorage.getItem(clave))) ;
+                    if (moviel.titulo == o.titulo) {
+                        flag = 1;
+                    }
+                }
+                if (flag==1) {
+                    alert("Esa pelicula ya esta cargada!!");
+                }
+                else {
+                    var index = len - 1;
+                    var key = parseInt(localStorage.key(index)) + 1;
+                    console.log("save");
+                    localStorage.setItem(key, JSON.stringify(o));
+                }
+            }
         }
     console.log(localStorage);
     },
@@ -50,7 +53,7 @@ var Movie = Backbone.Model.extend({
              if (x != localStorage.length-1) {
                  movielist =movielist  + ",";
              }
-            }
+        }
         movielist = movielist + "] }";
         return (JSON.parse(movielist));
     }
